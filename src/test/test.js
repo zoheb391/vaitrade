fd5565e7fdee21e15c325c2cd72be244ee21400c
zoheb salam <zoheb_391@hotmail.com>
2018-10-28 19:42:39 +0100

    basic test to calculate lexical density

---
+++ src/test/test.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+import chai from 'chai'
+import chaiHttp from 'chai-http'
+import server from '../server'
+
+let should = chai.should();
+chai.use(chaiHttp);
+
+describe('lexical density', () => {
+    describe('GET /complexity', () => {
+        it('it should calculate total lexical density', (done) => {
+            chai.request(server)
+                .post('/complexity')
+                .send({
+                    input: 'kim loves going to the cinema'
+                })
+                .end((err, res) => {
+                  res.body.data.total_ld.should.equal('0.67')
+                  res.should.have.status(200);
+                  done()
+                })
+        })
+    })
+  })
